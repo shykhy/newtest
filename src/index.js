c97d19c4629e15e31128b93de570e0e46e49bbdf
sean.shen <1846306921@qq.com>
2018-10-28 00:59:01 +0800

    m submit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 import dva from 'dva';
 import './index.css';
 
+sas
 mac update:
-this
+this as
 how are you
 
 this is key sy
 
-
 // 1. Initialize
 const app = dva();   
-
+hhh
 // 2. Plugins
 // app.use({}); 
 
 // 3. Model
 // app.model(require('./models/example').default); 
-
-asssss
-
-asas
+bbbb
 // 4. Router
 app.router(require('./router').default);
  
 // 5. Start
 app.start('#root');
 
-
-last one 
+last one
