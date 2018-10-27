d95b42e10f9ea4130e32956f89bad83a9fb1062f
sean.shen <1846306921@qq.com>
2018-10-28 00:53:04 +0800

    m submit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import './index.css';
 
 mac update:
 this
-how are you 
+how are you
+
+this is key sy
+
+
 // 1. Initialize
 const app = dva();   
 
@@ -12,9 +16,15 @@ const app = dva();
 
 // 3. Model
 // app.model(require('./models/example').default); 
-  
+
+asssss
+
+asas
 // 4. Router
 app.router(require('./router').default);
  
 // 5. Start
 app.start('#root');
+
+
+last one 
