a0bf5f001c0fc2cf5c5ce07f1ec197bc983bf3af
sean.shen <1846306921@qq.com>
2018-10-28 01:03:59 +0800

    w submit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import dva from 'dva';
 import './index.css';
 
-
-win update:
-this
-how are you
-
-this is key sy
-
-
+win update sssssss
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ asasas
 // 1. Initialize
 const app = dva();   
 
@@ -18,14 +27,9 @@ const app = dva();
 // 3. Model
 // app.model(require('./models/example').default); 
 
-asssss
-
-asas
 // 4. Router
 app.router(require('./router').default);
  
 // 5. Start
 app.start('#root');
 
-
-last one 
