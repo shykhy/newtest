1f0b076cd6d1a7f9befaf6093cd5f3ce7cd59a38
sean.shen <1846306921@qq.com>
2018-10-28 11:05:10 +0800

    update version shenhy

--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import dva from 'dva';
 import './index.css';
 
 win update sssssss
- 
-how are you 
-sas update:
-
-
 // 1. Initialize
 const app = dva();   
-hhhasas
 // 2. Plugins
 // app.use({}); 
 
 // 3. Model
 // app.model(require('./models/example').default); 
-
-bbbb
 
 // 4. Router
 app.router(require('./router').default);
@@ -24,4 +16,3 @@ app.router(require('./router').default);
 // 5. Start
 app.start('#root');
 last onea
-asas
